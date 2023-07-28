2e59e5662c97f6b521ac54fba7858c5b7dffd958
usulix <usulix@yahoo.com>
2023-07-28 10:13:53 -0700

    basic laravel 10 w/ tailwind no sail

---
+++ resources/views/pages/github.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('content')
+<div class="text-xl text-amber-800">
+    Github Here
+</div>
+@stop
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.default')
+@section('content')
+
+    <div class="text-xl text-center text-gray-900">
+        This is the home page
+    </div>
+    <a href="/github">GitHub Sample</a>
+@stop
